59af6575981a0f89f73f4b722643b9d83b2ea962
Islands <drislands@users.noreply.github.com>
2026-07-18 11:07:02 -0400

    Reorder Poker Hands
    The values (Two-Ace) in the Card Types module are arranged from low to high, but the Hands in the Poker Types module were arranged high to low. This resulted in incorrect comparisons for Hands that are the same type but have different high values. Fixed this by rearranging it so that Hands are also low to high.

--- src/Games/Poker/Types.hs
+++ src/Games/Poker/Types.hs
@@ -2,15 +2,15 @@ module Games.Poker.Types where
 
 import Cards.Types
 
-data Hand = RoyalFlush          --
-    | StraightFlush Value       -- High Card
-    | FourOfAKind Value         -- 
-    | FullHouse Value Value     -- Triplet, then Pair
-    | Flush [Value]             -- All five cards
-    | Straight Value            -- High Card
-    | ThreeOfAKind Value        --
-    | TwoPair Value Value Value -- High Pair, low Pair, 5th Card
+data Hand = HighCard [Value]          -- All five cards
     | Pair Value [Value]        -- Pair, then remaining Cards
-    | HighCard [Value]          -- All five cards
+    | TwoPair Value Value Value -- High Pair, low Pair, 5th Card
+    | ThreeOfAKind Value        -- The value you have 3 of
+    | Straight Value            -- High Card
+    | Flush [Value]             -- All five cards
+    | FullHouse Value Value     -- Triplet, then Pair
+    | FourOfAKind Value         -- The value you have 4 of
+    | StraightFlush Value       -- High Card
+    | RoyalFlush          -- All Royal Flushes tie.
     deriving (Eq, Ord, Show)
 
